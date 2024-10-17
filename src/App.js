df1e3169250f6bd40af9be706bf5fdd967a1ce91
Christopher Knight <110111479+KnightIdeas@users.noreply.github.com>
2024-10-17 14:45:53 +0100

    DrumMachine: Finalise css
    Get the drum machine centred
    Add the title and guidance text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import DrumMachine from "./components/DrumMachine";
 export default function App() {
   return (
     <div className="App">
+      <h2 className="title">Grid Based Drum Machine</h2>
       <DrumMachine />
+      <p className="guidance-text">Click on the drum buttons or press the corresponding keys to play drum samples</p>
     </div>
   );
 }
